13a9111e0611818dff71f84487e2fe8a686c7e53
geza-m <110627477+geza-m@users.noreply.github.com>
2023-01-19 18:23:41 +0100

    css completed as per excercises.

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+
+console.log('hey');
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+alert("oooh");
+
+for (let i = 0; i < answer; i++) {
+    if (i%3 == 0){
+        console.log(i);
+    }
+
+}
+
+
